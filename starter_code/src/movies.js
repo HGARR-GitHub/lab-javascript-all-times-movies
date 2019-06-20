3badd7679c291f728aa6658fa24620ce657c8522
hgarr <heriberto.garcia.ribas@gmail.com>
2019-06-20 18:50:33 -0500

    first commit, hours to minutes done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,47 @@
 /* eslint no-restricted-globals: 'off' */
+let updMovies = []
+
 // Turn duration of the movies from hours to minutes 
+
+const turnHoursToMinutes = (listMovies) => {
+  
+  updMovies = listMovies.map((movie) => {
+    
+    let hours = 0
+    let min = 0
+    let posH = 0
+    let posMin = 0
+    const minMovies = {}
+
+    //determine hours
+    posH = movie.duration.indexOf("h")
+    if (posH == -1){
+      hours = 0
+    } else {
+      hours = parseInt(movie.duration.substring(0, posH))
+    }
+
+    //determine minutes
+    posMin = movie.duration.indexOf("min")
+    if (posMin == -1){
+      min = 0
+    } else {
+      min = parseInt(movie.duration.substring(posH+1, posMin))
+    }
+    
+    minMovies.title = movie.title
+    minMovies.year = movie.year
+    minMovies.director = movie.director
+    minMovies.duration = (hours*60 + min).toString()
+    minMovies.genre = movie.genre
+    minMovies.rate = movie.rate
+
+    return minMovies
+  })
+  
+}
+
+turnHoursToMinutes(movies)
 
 
 // Get the average of all rates with 2 decimals 
